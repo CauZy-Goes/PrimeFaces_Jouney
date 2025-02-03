6ddd0e201a75c420c0b499b3b09ef39713aad269
cauzy_goes <cauafariasdev@gmail.com>
2025-02-02 21:09:12 -0300

    Inilized Project

---
+++ src/main/java/br/cauzy/jsf/controller/PessoaMB.java
@@ -0,0 +1,55 @@
+package br.cauzy.jsf.controller;
+
+
+import br.cauzy.jsf.model.Pessoa;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named("bean")
+@SessionScoped //aplicationScoped é compartilhado entre usuarios/navegaddores
+public class PessoaMB implements Serializable {
+    //aplicationScoped é compartilhado entre usuarios/navegaddores
+    //resquesSCcoped a classe é excluida no caso a lista quando é enviado ao servidor
+    // conversetion scope. injeta a variavel Conversation, conversation.begin() e convernsation.end(). comeca e finaliza o escopo
+
+    @Inject
+    private Pessoa pessoa ; // Iniciar diretamente a instância da pessoa.
+    private List<Pessoa> pessoas = new ArrayList<>();
+
+    // Adiciona a pessoa à lista e limpa o formulário
+    public String Adicionar() {
+        System.out.println("entrou");
+        pessoas.add(pessoa);
+        System.out.println(pessoa.getNome() + pessoa.getEmail());
+        Limpar();
+        System.out.println(pessoa.getNome() + pessoa.getEmail());
+        return "index";  // Aqui, faça a navegação para a página index.xhtml ou qualquer outro caminho
+    }
+
+    // Limpa o formulário (reseta a pessoa)
+    public void Limpar() {
+        this.pessoa = new Pessoa();  // Cria uma nova instância
+    }
+
+    // Getters e setters
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(List<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+}
---
+++ src/main/java/br/cauzy/jsf/model/Pessoa.java
@@ -0,0 +1,49 @@
+package br.cauzy.jsf.model;
+
+import jakarta.enterprise.context.Dependent;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Dependent
+public class Pessoa implements Serializable {
+    private Integer id;
+    private String nome;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(id, pessoa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
